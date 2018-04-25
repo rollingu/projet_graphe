c7f077b261caf9a7cc322b3dce9fdaa9ef086ef7
segauxv <segauxv@D9020M25327.phelma.grenoble-inp.fr>
2018-04-25 08:20:10 +0200

    Types.h

---
+++ types.h
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+
+typedef struct {     			// on définit un arc par son cout
+    int arrivee;			// et le numéro où il arrive.
+    double cout;
+} T_ARC;
+
+typedef struct lsucc {			// lsucc est une "liste" d'arc
+    T_ARC val;
+    struct lsucc* suiv;
+}* L_ARC;
+
+typedef struct {			// on définit un sommet par son
+    char* nom;				// nom et sa position
+    double x,y;				// il possède en plus une liste
+    L_ARC voisins;
+} T_SOMMET;				// d'arc.
+
+
+
+
