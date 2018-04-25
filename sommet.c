7ea4a77ff2ae472c6fafea160420247f263ae04e
rollingu <grollin69@gmail.com>
2018-04-25 10:16:51 +0200

    sommet

---
+++ sommet.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "sommet.h"
+
+void affiche_s(T_SOMMET s){
+
+
+}
---
+++ sommet.h
@@ -0,0 +1,14 @@
+#ifndef _SOMMET_H_H
+#define _SOMMET_H_H
+
+
+typedef struct {			// on définit un sommet par son
+    char* nom;				// nom et sa position
+    double x,y;				// il possède en plus une liste
+    L_ARC voisins;
+} T_SOMMET;				// d'arc.
+
+
+void afficher_s(T_SOMMET s);
+
+#endif
